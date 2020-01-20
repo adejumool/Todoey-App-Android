66c1894cc02cea06a001714e7a95d02a0c26e217
Tobi <adejumool@berea.edu>
2020-01-19 23:32:20 -0500

    adding almost complete todo list app

--- app/src/main/java/com/example/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -1,14 +1,88 @@
 package com.example.simpletodo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.os.FileUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    List<String> items;
+    Button btnAdd;
+    EditText etItem;
+    RecyclerView rvItem;
+
+    ItemsAdapter itemsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        btnAdd = findViewById(R.id.btnAdd);
+        etItem = findViewById(R.id.etItem);
+        rvItem = findViewById(R.id.rvItem);
+
+        etItem.setText("HaioR");
+
+
+        items = new ArrayList<>();
+        items.add("milk");
+        items.add("Go to gym");
+        items.add("talk to John");
+
+        ItemsAdapter.onLongClickListener onLongClickListener= new ItemsAdapter.onLongClickListener(){
+            @Override
+            public void onItemLongClicked(int position) {
+                items.remove(position);
+                itemsAdapter.notifyItemRemoved(position );
+                Toast.makeText(getApplicationContext(), "item has been removed", Toast.LENGTH_LONG).show();
+            }
+        };
+
+        itemsAdapter = new ItemsAdapter(items, onLongClickListener);
+        rvItem.setAdapter(itemsAdapter);
+        rvItem.setLayoutManager(new LinearLayoutManager(this));
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String todoItem = etItem.getText().toString();
+
+                items.add(todoItem);
+
+                itemsAdapter.notifyItemInserted(items.size()-1);
+
+                etItem.setText("");
+                Toast.makeText(getApplicationContext(), "item was added", Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+    private File getDataFile(){
+        return new File(getFilesDir(),"data.text");
+    }
+
+    private void loadItems(){
+        try{
+            items = new ArrayList<>(FileUtils.readLines(getDataFile(),Charset.defaultCharset()));
+        }catch (IOException e){
+            Log.e("MainActivity", "Error reading items", e);
+            items = new ArrayList<>();
+        }
     }
 }
